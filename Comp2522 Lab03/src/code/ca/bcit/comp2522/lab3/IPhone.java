2f1e297920ddb414bc2246b3d9b8ae4bfc6f6734
Dalraj Bains <dbains38@my.bcit.ca>
2024-09-24 10:22:08 -0700

    (Iphone + Iphone16 classes added)

--- Comp2522 Lab03/src/code/ca/bcit/comp2522/lab3/IPhone.java
+++ Comp2522 Lab03/src/code/ca/bcit/comp2522/lab3/IPhone.java
@@ -1,4 +1,120 @@
 package ca.bcit.comp2522.lab3;
 
-public class IPhone extends IDevice{
+import java.util.Objects;
+
+/**
+ * Represents an iPhone device.
+ *
+ * @author Dalraj Bains
+ * @author Anil Bronson
+ * @author Farzad Farzin
+ * @version 1.0
+ */
+public class IPhone extends IDevice {
+
+  private double minutesRemaining;
+  private String carrier;
+
+  /**
+   * Constructs an IPhone with specified minutes remaining and carrier.
+   *
+   * @param minutesRemaining the number of minutes remaining on the phone plan
+   * @param carrier the carrier of the phone
+   */
+  public IPhone(double minutesRemaining,
+                String carrier) {
+
+    // Set the purpose of the device to "talking"
+    super("talking");
+    this.minutesRemaining = minutesRemaining;
+    this.carrier = carrier;
+  }
+
+  /**
+   * Gets the minutes remaining on the phone plan.
+   *
+   * @return the minutes remaining
+   */
+  public double getMinutesRemaining() {
+    return minutesRemaining;
+  }
+
+  /**
+   * Sets the minutes remaining on the phone plan.
+   *
+   * @param minutesRemaining the new minutes remaining
+   */
+  public void setMinutesRemaining(double minutesRemaining) {
+    this.minutesRemaining = minutesRemaining;
+  }
+
+  /**
+   * Gets the carrier of the phone.
+   *
+   * @return the carrier
+   */
+  public String getCarrier() {
+    return carrier;
+  }
+
+  /**
+   * Sets the carrier of the phone.
+   *
+   * @param carrier the new carrier
+   */
+  public void setCarrier(String carrier) {
+    this.carrier = carrier;
+  }
+
+  /**
+   * Prints the details of the iPhone, including minutes remaining and carrier.
+   */
+  @Override
+  public void printDetails() {
+
+    System.out.println("Minutes Remaining: " +
+            minutesRemaining);
+
+    System.out.println("Carrier: " +
+            carrier);
+  }
+
+  /**
+   * Returns a string representation of the iPhone, including all instance variables.
+   *
+   * @return a string with all instance variables
+   */
+  @Override
+  public String toString() {
+    return super.toString() +
+            ", Minutes Remaining: " +
+            minutesRemaining +
+            ", Carrier: " +
+            carrier;
+  }
+
+  /**
+   * Checks if this iPhone is equal to another object based on minutes remaining.
+   *
+   * @param obj the object to compare with
+   * @return true if the objects are equal, false otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    IPhone iPhone = (IPhone) obj;
+    return Double.compare(iPhone.minutesRemaining, minutesRemaining) == 0;
+  }
+
+  /**
+   * Returns the hash code of the iPhone based on minutes remaining.
+   *
+   * @return the hash code
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(minutesRemaining);
+  }
 }
---
+++ Comp2522 Lab03/src/code/ca/bcit/comp2522/lab3/IPhone16.java
@@ -0,0 +1,128 @@
+package ca.bcit.comp2522.lab3;
+
+import java.util.Objects;
+
+/**
+ * Represents an iPhone 16 device which is extending the IPhone class.
+ *
+ * @author Dalraj Bains
+ * @author Anil Bronson
+ * @author Farzad Farzin
+ * @version 1.0
+ */
+public class IPhone16 extends IPhone {
+
+  private boolean highResolutionCamera;
+  private int gigabytesOfMemory;
+
+  /**
+   * Constructs an IPhone16 with specified attributes.
+   *
+   * @param minutesRemaining the number of minutes remaining on the phone plan
+   * @param carrier the carrier of the phone
+   * @param highResolutionCamera whether the phone has a high-resolution camera
+   * @param gigabytesOfMemory the amount of memory in gigabytes
+   */
+  public IPhone16(double minutesRemaining,
+                  final String carrier,
+                  boolean highResolutionCamera,
+                  int gigabytesOfMemory) {
+
+    // Call the parent constructor
+    super(minutesRemaining, carrier);
+    this.highResolutionCamera = highResolutionCamera;
+    this.gigabytesOfMemory = gigabytesOfMemory;
+  }
+
+  /**
+   * Checks if the phone has a high-resolution camera.
+   *
+   * @return true if the phone has a high-resolution camera, false otherwise
+   */
+  public boolean hasHighResolutionCamera() {
+    return highResolutionCamera;
+  }
+
+  /**
+   * Sets the high-resolution camera status.
+   *
+   * @param highResolutionCamera the new camera status
+   */
+  public void setHighResolutionCamera(boolean highResolutionCamera) {
+    this.highResolutionCamera = highResolutionCamera;
+  }
+
+  /**
+   * Gets the amount of memory in gigabytes.
+   *
+   * @return the memory in gigabytes
+   */
+  public int getGigabytesOfMemory() {
+    return gigabytesOfMemory;
+  }
+
+  /**
+   * Sets the amount of memory in gigabytes.
+   *
+   * @param gigabytesOfMemory the new memory amount
+   */
+  public void setGigabytesOfMemory(int gigabytesOfMemory) {
+    this.gigabytesOfMemory = gigabytesOfMemory;
+  }
+
+  /**
+   * Prints the details of the iPhone 16, including high-resolution camera and memory.
+   */
+  @Override
+  public void printDetails() {
+
+    // Print details from the parent class
+    super.printDetails();
+
+    System.out.println("High Resolution Camera: " +
+            highResolutionCamera);
+
+    System.out.println("Gigabytes of Memory: " +
+            gigabytesOfMemory);
+  }
+
+  /**
+   * Returns a string representation of the iPhone 16, including all instance variables.
+   *
+   * @return a string with all instance variables
+   */
+  @Override
+  public String toString() {
+    return super.toString() +
+            ", High Resolution Camera: " +
+            highResolutionCamera +
+            ", Gigabytes of Memory: " +
+            gigabytesOfMemory;
+  }
+
+  /**
+   * Checks if this iPhone 16 is equal to another object based on minutes remaining and high-resolution camera.
+   *
+   * @param obj the object to compare with
+   * @return true if the objects are equal, false otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    if (!super.equals(obj)) return false;
+    IPhone16 iPhone16 = (IPhone16) obj;
+    return highResolutionCamera == iPhone16.highResolutionCamera;
+  }
+
+  /**
+   * Returns the hash code of the iPhone 16 based on minutes remaining and high-resolution camera.
+   *
+   * @return the hash code
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), highResolutionCamera);
+  }
+}
